de9ca1db36a23052252c5fe51fd95a16820fcf0c
Gui <guilherme.rddev@gmail.com>
2022-06-03 15:59:05 -0300

    first commit

---
+++ src/main/java/br/com/gui/aws_app02/AwsApp02Application.java
@@ -0,0 +1,13 @@
+package br.com.gui.aws_app02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsApp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsApp02Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gui/aws_app02/AwsApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gui.aws_app02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
