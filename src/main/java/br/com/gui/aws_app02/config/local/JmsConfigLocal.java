8e66866d7165fa023d84bc0a00be15e6c79013a2
Gui <guilherme.rddev@gmail.com>
2022-06-03 21:44:30 -0300

    feature: adiciona configuracao local para fila sqs

--- src/main/java/br/com/gui/aws_app02/config/local/JmsConfigLocal.java
+++ src/main/java/br/com/gui/aws_app02/config/local/JmsConfigLocal.java
@@ -1,9 +1,11 @@
-package br.com.gui.aws_app02.config;
+package br.com.gui.aws_app02.config.local;
 
 import com.amazon.sqs.javamessaging.ProviderConfiguration;
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -16,8 +18,8 @@ import javax.jms.Session;
 
 @Service
 @EnableJms
-@Profile("!local")
-public class JmsConfig {
+@Profile("local")
+public class JmsConfigLocal {
 
   @Value("${aws.region}")
   private String awsRegion;
@@ -27,8 +29,10 @@ public class JmsConfig {
   @Bean
   public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
     sqsConnectionFactory = new SQSConnectionFactory(new ProviderConfiguration(),
-        AmazonSQSClientBuilder.standard()
-            .withRegion(awsRegion)
+        AmazonSQSClient.builder().withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                "http://localhost:4566",
+                Regions.SA_EAST_1.getName()
+            ))
             .withCredentials(new DefaultAWSCredentialsProviderChain())
             .build());
     final var factory = new DefaultJmsListenerContainerFactory();
